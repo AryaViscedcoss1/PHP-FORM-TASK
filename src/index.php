ca3c899d8a2a724fd98dc7310c409cdf6c3a8c90
Arya Vishwakarma <98632890+AryaViscedcoss1@users.noreply.github.com>
2022-02-25 14:32:12 +0530

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,55 +1,66 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>template</title>
-<link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <form action="" method="post">
-<input type="file" name="fileToUpload" id="fileToUpload">
+Units <input type="number" name="bill"><br>
 <input type="submit" name="submit"><br>
 </form> 
-<?php
-$target_dir = "upload/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+<?php  
+  $b1=0;
+  $b2=0;
+  $b3=0;
+  $b4=0;
+  $b=$_POST["bill"];
+ if (isset($_POST["submit"]))
+ {
+    if($b<=50){
+      $b1=($b)*3.5;
+      $t=$b1+$b2+$b3+$b4;
+      echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
+      echo "Total amount ".$t." </h1>";
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-  }
-  
-  // Check file size
-  if ($_FILES["fileToUpload"]["size"] > 200000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-  }
-  
-  // Allow certain file formats
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-  }
-  if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-  // if everything is ok, try to upload file
-  } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    } else {
-      echo "Sorry, there was an error uploading your file.";
     }
+
+
+    elseif($b>50 && $b<=150)
+      {
+        $b2=($b-50)*4;
+        $b1=50*3.5;
+        $t=$b1+$b2;
+        echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
+        echo "<h1>For next 100 units is Rs. 4.00/unit ".$b2." </h1>"; 
+        echo "Total amount ".$t." </h1>";
+      }
+    
+
+      elseif($b>150 && $b<=250)
+      {
+        $b2=($b-100)*4;
+        $b1=50*3.5;
+        $b3=($b-150)*5.2;
+        $t=$b1+$b2+$b3;
+        echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
+        echo "<h1>For next 100 units is Rs. 4.00/unit ".$b2." </h1>"; 
+        echo "<h1>For next 100 units is Rs. 5.20/unit ".$b3." </h1>"; 
+        echo "Total amount ".$t." </h1>";
+      }
+
+    
+      elseif($b>=250)
+      {
+        $b2=($b-100)*4;
+        $b1=50*3.5;
+        $b3=($b-150)*5.2;
+        $b4=($b-250)*6.5;
+        $t=$b1+$b2+$b3+$b4;
+        echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
+        echo "<h1>For next 100 units is Rs. 4.00/unit ".$b2." </h1>"; 
+        echo "<h1>For next 100 units is Rs. 5.20/unit ".$b3." </h1>"; 
+        echo "<h1>For next 100 units is Rs. 5.20/unit ".$b4." </h1>"; 
+        echo "Total amount ".$t." </h1>";
+
+      }
   }
-  
-}
+
 
 ?>
 </body>
-</html>
-<script src="script.js "></script>
-</body>  
-</html>  
