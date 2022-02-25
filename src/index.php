49f115b39a88604e624539fdbc72bd694137b97c
Arya Vishwakarma <98632890+AryaViscedcoss1@users.noreply.github.com>
2022-02-25 14:40:09 +0530

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,66 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>template</title>
+<link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <form action="" method="post">
-Units <input type="number" name="bill"><br>
-<input type="submit" name="submit"><br>
+Number1: <input type="number" name="number1" ><br><br>
+Number2: <input type="number" name="number2" min="1"><br><br>
+Result <input type="number" name="result" value="<?php echo cal() ?>"><br><br>
+<input type="submit" value="+" name="submit1">
+<input type="submit" value="-" name="submit2">
+<input type="submit" value="*" name="submit3">
+<input type="submit" value="/" name="submit4"><br>
 </form> 
 <?php  
-  $b1=0;
-  $b2=0;
-  $b3=0;
-  $b4=0;
-  $b=$_POST["bill"];
- if (isset($_POST["submit"]))
- {
-    if($b<=50){
-      $b1=($b)*3.5;
-      $t=$b1+$b2+$b3+$b4;
-      echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
-      echo "Total amount ".$t." </h1>";
+function cal()
+{
 
-    }
+$num1=$_POST["number1"];
+$num2=$_POST["number2"];
+$result=$_POST["result"];
+if(isset($_POST["submit1"]))
+{
+   $result=$num1+$num2;
+}
+elseif(isset($_POST["submit2"]))
+{
+  $result=$num1-$num2;
+} 
+elseif(isset($_POST["submit3"]))
+{
+  $result=$num1*$num2;
+} 
+elseif(isset($_POST["submit4"]))
+{
+  $result=$num1/$num2;
+}
 
+return $result;
 
-    elseif($b>50 && $b<=150)
-      {
-        $b2=($b-50)*4;
-        $b1=50*3.5;
-        $t=$b1+$b2;
-        echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
-        echo "<h1>For next 100 units is Rs. 4.00/unit ".$b2." </h1>"; 
-        echo "Total amount ".$t." </h1>";
-      }
-    
-
-      elseif($b>150 && $b<=250)
-      {
-        $b2=($b-100)*4;
-        $b1=50*3.5;
-        $b3=($b-150)*5.2;
-        $t=$b1+$b2+$b3;
-        echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
-        echo "<h1>For next 100 units is Rs. 4.00/unit ".$b2." </h1>"; 
-        echo "<h1>For next 100 units is Rs. 5.20/unit ".$b3." </h1>"; 
-        echo "Total amount ".$t." </h1>";
-      }
-
-    
-      elseif($b>=250)
-      {
-        $b2=($b-100)*4;
-        $b1=50*3.5;
-        $b3=($b-150)*5.2;
-        $b4=($b-250)*6.5;
-        $t=$b1+$b2+$b3+$b4;
-        echo "<h1>For first 50 units is Rs. 3.50/unit ".$b1." </h1>";
-        echo "<h1>For next 100 units is Rs. 4.00/unit ".$b2." </h1>"; 
-        echo "<h1>For next 100 units is Rs. 5.20/unit ".$b3." </h1>"; 
-        echo "<h1>For next 100 units is Rs. 5.20/unit ".$b4." </h1>"; 
-        echo "Total amount ".$t." </h1>";
-
-      }
-  }
-
+}
 
 ?>
 </body>
+</html>
+<script src="script.js "></script>
--- src/script.js
+++ src/script.js
@@ -1,3 +1,96 @@
+// variable intialization
+var add=document.getElementById("ADD");
+var dId=document.getElementById("displayId");
+var dName=document.getElementById("displayName");
+var dPrice=document.getElementById("displayPrice");
+var dUpdate=document.getElementById("displayUpdate");
+var tophead=document.getElementById("topheading");
+
+var data=[];
+
+// logic!!
+
+
+function display()
+{    
+    dId.innerHTML="";
+    dName.innerHTML="";
+    dPrice.innerHTML="";
+
+    console.log(data)
+    data.forEach(function myFunc(value,index,array){
+          dId.innerHTML+='<br>'+array[index].Id;
+          dName.innerHTML+='<br>'+array[index].name;
+          dPrice.innerHTML+='<br>'+array[index].price;
+          dUpdate.innerHTML+='<br>'
+         
+
+    });
+}
+
+
+
+ // onclick event
+
+ //edit
+
+
+ add.addEventListener("click",function myFunc(){
+   
+  // value extraction 
+    var id=document.getElementById("id").value;
+    var Name=document.getElementById("Name").value;
+    var Price=document.getElementById("Price").value;
+ 
+  //DEFINITION OF EDIT
+    var edit=document.createElement("button");
+    edit.innerHTML="edit";
+    dUpdate.appendChild(edit);
+    edit.addEventListener("click",function editFunc()
+    {
+     console.log(edit);
+     tophead.innerHTML="this is object "+id+" "+Name;
+    });
+  //DEFINITION OF DELETE
+    var deletes=document.createElement("button");
+    deletes.innerHTML="delete";
+
+
+    
+    // value to send for process
+
+    dUpdate.appendChild(deletes);
+    console.log("values to be sended for processing");
+    console.log(id,Name,Price);
+ 
+    if (data.length===0)
+    {
+        data.push({'Id':id,'name':Name,'price':Price,'edit':edit,'deletes':deletes});
+        display();
+    }
+    else if (check(id))
+    {
+        data.push({'Id':id,'name':Name,'price':Price,'edit':edit,'deletes':deletes});
+        display();
+
+    }
+
+
+ });
+ // check function
+ function check(x)
+ {
+    for(i=0;i<data.length;i++)
+        
+    {    
+        if (x == data[i].Id)
+        {
+           return false;
+        }
+    }
+    return true;
+
+ }
 
  
 
